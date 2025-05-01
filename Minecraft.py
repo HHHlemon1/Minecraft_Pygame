cea4696a26c8cd220ffe4a2f8d168fc25b8f0aec
HHHlemon1 <HHH.lemon1@gmail.com>
2025-05-01 11:16:11 -0700

    Update Minecraft.py

--- Minecraft.py
+++ Minecraft.py
@@ -1,4 +1,36 @@
 import pygame
 
+pygame.init()
+resolution = (1920,1080)
+win = pygame.display.set_mode(resolution)
+pygame.display.set_caption("Minecraft")
+
+player_width,player_height = 40,60
+
+
+
+class player:
+    def __init__(self):
+        
 
 print("Hello Minecraft")
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill background
+    win.fill((135, 206, 235))
+
+    # Get pressed keys
+    #keys = pygame.key.get_pressed()
+
+    # Move and draw the player
+    #player.move(keys)
+    #player.draw(win)
+
+    pygame.display.flip()
+
+pygame.quit()
